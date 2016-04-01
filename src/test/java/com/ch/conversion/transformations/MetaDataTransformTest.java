af9239d4ef3c27a662369d94438a2cfba19fba4e
Elliott Jenkins <elliott.jenkins@methods.co.uk>
2016-04-01 14:45:30 +0100

    MetaDataTransform tests

--- src/test/java/com/ch/conversion/transformations/MetaDataTransformTest.java
+++ src/test/java/com/ch/conversion/transformations/MetaDataTransformTest.java
@@ -2,7 +2,13 @@ package com.ch.conversion.transformations;
 
 import com.ch.conversion.config.ITransformConfig;
 import com.ch.conversion.config.TestTransformationConfig;
+import com.ch.conversion.config.TransformConfig;
+import com.ch.helpers.TestHelper;
+import org.hamcrest.CoreMatchers;
+import org.json.JSONObject;
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 
 import java.io.IOException;
 
@@ -14,7 +20,24 @@ public class MetaDataTransformTest {
   ITransformConfig config;
 
   @Before
-  public void setUp() throws IOException {
-    config = new TestTransformationConfig();
+  public void setUp() {
+    config = new TransformConfig();
+  }
+
+  @Test
+  public void addMetaDataToRootXmlElement() throws Exception {
+    // xml
+    String xml_path = "src/test/resources/form.xml";
+    String xml = TestHelper.getStringFromFile(xml_path);
+
+    // meta json
+    String meta_path = "src/test/resources/meta.json";
+    String meta_string = TestHelper.getStringFromFile(meta_path);
+    JSONObject meta_json = new JSONObject(meta_string);
+
+    MetaDataTransform transform = new MetaDataTransform(config, xml, meta_json);
+    String output = transform.getXml();
+
+    Assert.assertThat(output, CoreMatchers.containsString("<?xml version='1.0'?><form type='SH08' version=''>"));
   }
 }
