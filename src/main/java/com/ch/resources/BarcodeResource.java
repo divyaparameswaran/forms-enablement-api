ba70c66e5c8a2f2313719f4584450a390ae32ad0
Aaron Witter <aaronwitter@hotmail.com>
2016-04-04 09:43:51 +0100

    added barcode resource

---
+++ src/main/java/com/ch/resources/BarcodeResource.java
@@ -0,0 +1,45 @@
+package com.ch.resources;
+
+import com.ch.application.FormsServiceApplication;
+import com.ch.configuration.CompaniesHouseConfiguration;
+import com.codahale.metrics.Timer;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ * Created by Aaron.Witter on 01/04/2016.
+ */
+@Path("/barcode")
+public class BarcodeResource {
+  private static final Logger log = LogManager.getLogger(BarcodeResource.class);
+  private static final Timer timer = FormsServiceApplication.registry.timer("BarcodeResource");
+
+  private final Client client;
+  private final CompaniesHouseConfiguration configuration;
+
+  public BarcodeResource(Client client, CompaniesHouseConfiguration configuration) {
+    this.client = client;
+    this.configuration = configuration;
+  }
+
+  @POST
+  @Consumes(MediaType.APPLICATION_JSON)
+  @Produces(MediaType.APPLICATION_JSON)
+  public Response getBarcode(String dateReceived){
+    // post to CHIPS
+    final WebTarget target = client.target(configuration.getBarcodeServiceUrl());
+    // return response from CHIPS
+    Response response = target.request().post(Entity.entity(dateReceived, MediaType.APPLICATION_JSON_TYPE));
+    return response;
+  }
+}
