fac390d4f6ba3d362cd2c3343c53718186261033
Elliott Jenkins <elliott.jenkins@methods.co.uk>
2016-04-06 09:29:15 +0100

    javadoc

--- src/main/java/com/ch/exception/MissingRequiredDataException.java
+++ src/main/java/com/ch/exception/MissingRequiredDataException.java
@@ -10,12 +10,25 @@ public class MissingRequiredDataException extends WebApplicationException {
   private final String property;
   private final String parent;
 
+  /**
+   * Exception thrown when required data is not present.
+   *
+   * @param exception error
+   * @param property property not found
+   * @param parent where it should be
+   */
   public MissingRequiredDataException(Exception exception, String property, String parent) {
     super(exception);
     this.property = property;
     this.parent = parent;
   }
 
+  /**
+   * Exception thrown when required data is not present.
+   *
+   * @param property property not found
+   * @param parent where it should be
+   */
   public MissingRequiredDataException(String property, String parent) {
     this.property = property;
     this.parent = parent;
