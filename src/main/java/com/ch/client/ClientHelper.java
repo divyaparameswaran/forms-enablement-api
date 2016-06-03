185d94d2452cd357b4a4344616b7e9d5b7286b35
Aaron Witter <aaronwitter@hotmail.com>
2016-06-03 16:14:03 +0100

    new methods to get token from salesforce, and post to salesforce client along with verdict from examiner

--- src/main/java/com/ch/client/ClientHelper.java
+++ src/main/java/com/ch/client/ClientHelper.java
@@ -1,8 +1,11 @@
 package com.ch.client;
 
+import com.ch.configuration.SalesforceConfiguration;
+
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 /**
@@ -27,4 +30,36 @@ public final class ClientHelper {
     final WebTarget target = client.target(url);
     return target.request().post(Entity.json(json));
   }
+
+  /**
+   * Gets token from Salesforce oauth client.
+   *
+   * @param configuration Salesforce configuration object
+   * @return response from url
+   */
+  public Response getToken(SalesforceConfiguration configuration) {
+    final WebTarget target = client.target(configuration.getAuthUrl())
+        .queryParam("grant_type", configuration.getAuthGrantType())
+        .queryParam("client_id", configuration.getAuthId())
+        .queryParam("client_secret", configuration.getAuthSecret())
+        .queryParam("username", configuration.getAuthUsername())
+        .queryParam("password", configuration.getAuthPassword());
+    return target.request().post(Entity.json(null));
+  }
+
+  /**
+   * Posts json verdict to Salesforce.
+   *
+   * @param clientUrl   salesforce client url
+   * @param accessToken access token
+   * @param verdict     json verdict from chips
+   * @return response
+   */
+  public Response postVerdict(String clientUrl, String accessToken, String verdict) {
+    final WebTarget target = client.target(clientUrl);
+    return target.request(MediaType.APPLICATION_XML_TYPE)
+        .header("Authorization", "Bearer " + accessToken).method("PATCH", Entity.json(verdict));
+  }
 }
+
+
--- src/main/java/com/ch/resources/FormResponseResource.java
+++ src/main/java/com/ch/resources/FormResponseResource.java
@@ -9,6 +9,7 @@ import com.ch.configuration.SalesforceConfiguration;
 import com.ch.service.LoggingService;
 import com.codahale.metrics.Timer;
 import io.dropwizard.auth.Auth;
+import org.json.simple.JSONObject;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
@@ -43,14 +44,23 @@ public class FormResponseResource {
   @Consumes(MediaType.APPLICATION_JSON)
   @Produces(MediaType.APPLICATION_JSON)
   public Response postResponse(@Auth
-                                   String verdict) {
+                               String verdict) {
     final Timer.Context context = timer.time();
     try {
       LoggingService.log(tag, INFO, "Verdict from CHIPS: " + verdict,
           FormResponseResource.class);
 
+      //Get Token from Salesforce
+      Response tokenReponse = getToken();
+
+      JSONObject accessTokenJson = tokenReponse.readEntity(JSONObject.class);
+
+      LoggingService.log(tag, INFO, "Token from Salesforce: " + accessTokenJson.toJSONString(), FormResponseResource.class);
+
+      String accessToken = (String) accessTokenJson.get("access_token");
+
       // POST to Salesforce
-      Response response = client.postJson(configuration.getApiUrl(), verdict);
+      Response response = client.postVerdict(configuration.getClientUrl(), accessToken, verdict);
       LoggingService.log(tag, INFO, "Response from Salesforce " + response,
           FormResponseResource.class);
       return response;
@@ -58,5 +68,9 @@ public class FormResponseResource {
     } finally {
       context.stop();
     }
+  }
+
+  private Response getToken() {
+    return client.getToken(configuration);
   }
 }
