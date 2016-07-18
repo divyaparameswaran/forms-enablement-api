70ca5c9cc569f20ed72224c7f63a534c92cf7a53
Aaron Witter <aaronwitter@hotmail.com>
2016-07-18 15:48:33 +0100

    newly modelled queue request for activator, beginning on mongo helper test

--- src/main/java/com/ch/model/QueueRequest.java
+++ src/main/java/com/ch/model/QueueRequest.java
@@ -4,4 +4,31 @@ package com.ch.model;
  * Created by Aaron.Witter on 18/07/2016.
  */
 public class QueueRequest {
+
+    private String formStatus;
+    private long count;
+
+
+    /**
+     * Constructs an empty queue request.
+     */
+    public QueueRequest() {
+    //Empty constructor is needed for automatic deserialisation
+    }
+
+    /**
+     * Constructs an empty queue request.
+     */
+    public QueueRequest(String formStatus, long count) {
+        this.formStatus = formStatus;
+        this.count = count;
+    }
+
+    public String getFormStatus() {
+        return formStatus;
+    }
+
+    public long getCount() {
+        return count;
+    }
 }
--- src/test/java/com/ch/helpers/MongoHelperTest.java
+++ src/test/java/com/ch/helpers/MongoHelperTest.java
@@ -1,7 +1,78 @@
 package com.ch.helpers;
 
+import com.ch.application.FormsServiceApplication;
+import com.ch.configuration.FormsServiceConfiguration;
+import com.ch.conversion.builders.JsonBuilder;
+import com.ch.conversion.config.ITransformConfig;
+import com.ch.conversion.config.TransformConfig;
+import com.ch.cucumber.FormServiceTestSuiteIT;
+import com.ch.model.FormsPackage;
+import com.mongodb.client.FindIterable;
+import io.dropwizard.testing.ResourceHelpers;
+import io.dropwizard.testing.junit.DropwizardAppRule;
+import org.bson.Document;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Aaron.Witter on 18/07/2016.
  */
-public class MongoHelperTest {
+public class MongoHelperTest extends TestHelper{
+
+    private MongoHelper helper;
+    ITransformConfig config;
+
+    @ClassRule
+    public static final DropwizardAppRule<FormsServiceConfiguration> RULE =
+        new DropwizardAppRule<>(FormsServiceApplication.class, ResourceHelpers.resourceFilePath
+            ("test-configuration.yml"));
+
+    @Before
+    public void setUp() {
+        config = new TransformConfig();
+        MongoHelper.init(FormServiceTestSuiteIT.RULE.getConfiguration());
+        helper =  MongoHelper.getInstance();
+    }
+    
+    @Test
+    public void shouldReturnResultsOldestFirst() throws IOException {
+
+        // valid package data
+        String package_string = getStringFromFile(PACKAGE_JSON_PATH);
+        // valid forms
+        String valid = getStringFromFile(FORM_ALL_JSON_PATH);
+        List<String> valid_forms = new ArrayList<>();
+        for (int i = 0; i < 2; i++) {
+            valid_forms.add(valid);
+        }
+        // builder
+        FormsPackage formsPackage = new JsonBuilder(config, package_string, valid_forms).getTransformedPackage();
+
+        helper.storeFormsPackage(formsPackage);
+
+        // valid package data
+        String package_string2 = getStringFromFile(SINGLE_PACKAGE_JSON_PATH);
+        // valid forms
+        String valid2 = getStringFromFile(FORM_ALL_JSON_PATH);
+        List<String> valid_forms2 = new ArrayList<>();
+        for (int i = 0; i < 1; i++) {
+            valid_forms2.add(valid2);
+        }
+        // builder
+        FormsPackage formsPackage2 = new JsonBuilder(config, package_string2, valid_forms).getTransformedPackage();
+
+        helper.storeFormsPackage(formsPackage2);
+
+        FindIterable<Document> documents = helper.getPackagesCollectionByStatus("PENDING", 2);
+
+        Assert.assertTrue(documents.first().get("packageIdentifier").equals("12345"));
+
+    }
+
 }
