97a68582eb843142d6e121efe8d201ca5212c2c3
Aaron Witter <aaronwitter@hotmail.com>
2016-08-02 12:18:54 +0100

    new classes for connecting to presenter auth api and modelling it's requests and testing

---
+++ src/main/java/com/ch/client/PresenterHelper.java
@@ -0,0 +1,7 @@
+package com.ch.client;
+
+/**
+ * Created by Aaron.Witter on 01/08/2016.
+ */
+public class PresenterHelper {
+}
---
+++ src/main/java/com/ch/model/PresenterAuthRequest.java
@@ -0,0 +1,7 @@
+package com.ch.model;
+
+/**
+ * Created by Aaron.Witter on 01/08/2016.
+ */
+public class PresenterAuthRequest {
+}
